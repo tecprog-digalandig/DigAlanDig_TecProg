5d87606954149ce3830794e455b5a65c5d1ebe28
ingr.dcg@gmail.com <ingr.dcg@gmail.com>
2018-09-20 15:12:50 -0300

    [CODE] Commenting on resources.h

--- include/Resources.h
+++ include/Resources.h
@@ -11,26 +11,41 @@
 
 // Resources Class characteristics and heritages.
 class Resources 
+// Resource now is going to work with allocations and deallocations.
 {
   public:
+    // Manage textures.
+    // Once the texture is allocated, "get" functions will work as a call instead of a method to load texture.
     static std::shared_ptr<sdl_texture> getImage(const std::string& file);
     static void clearImages();
-
+    // Runs through the image table destroying texture by texture until empty the table.
+    
     static std::shared_ptr<mix_music> getMusic(const std::string& file);
     static void clearMusics();
+    // Runs through the music table destroying texture by texture until empty the table.
 
     static std::shared_ptr<mix_chunk> getSound(const std::string& file);
     static void clearSounds();
+    // Runs through the sound table destroying texture by texture until empty the table.
 
     static void clearAll();
+    // Clear all the table (Great option to free some game memory during the program).
 
   private:
+    // "shared_ptr" is used to count users from a resource.
+    // "unordered_map" is a hash table (template) which receives to parameters.
+    // The parameters are a type used as key to the table and content pointed from the key.
     static std::unordered_map<std::string, std::shared_ptr<sdl_texture>>
         image_table;
+        // "image_table" is associed a string to a texture pointer.
     static std::unordered_map<std::string, std::shared_ptr<mix_music>>
         music_table;
+        // "music_table" is associed a string to a texture pointer.
     static std::unordered_map<std::string, std::shared_ptr<mix_chunk>>
         sound_table;
+        // "sound_table" is associed a string to a texture pointer.
+
+    // Everytime is charged a new texture, it's kept in a table so it won't be necessary to load twice.
 };
 
 #endif  // RESOURCES_H
