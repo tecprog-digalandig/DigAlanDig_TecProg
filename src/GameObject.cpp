bca062f070a3eb629352cf6e98a2f63a771f6703
Raul Victor <rvictors@hotmail.com>
2018-11-12 18:27:59 -0200

    [IMPORTANT FIRST] Moving more important methods to the top

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,55 +1,94 @@
+/**
+* @file GameObject.cpp
+* @copyright 2018 Pedro H.
+* @brief Class that create, move, update and render objects on the screen.
+*/
+
 #include "GameObject.h"
 #include "Sound.h"
+#include <assert.h>
 
-bool GameObject::CanEnd() const { return true; }
-
-GameObject::~GameObject() {
+GameObject::~GameObject()
+{
     for (Component* component : components) delete (component);
 
     components.clear();
     tmp = box;
 }
 
-void GameObject::update(float dt) {
-    static float sum;
-    sum += dt;
-    for (Component* component : components) component->update(dt);
-    if (move) {
-        box.pos = tmp.pos + Vec2(0, 1) * sin(sum) * 30;
+void GameObject::start()
+{
+    if (started)
+    {
+        return;
     }
-}
-
-void GameObject::rhythmUpdate() {
-    for (Component* component : components) component->rhythmUpdate();
-}
-
-void GameObject::RhythmReset() {
-    for (Component* component : components) component->RhythmReset();
-}
-
-void GameObject::RenderOrder(Common::Layer layer) const {
-    if (!(this->layer & layer)) return;
-
-    for (Component* component : components) component->render(layer);
-}
-
-void GameObject::RemoveComponent(Component* cpt) {
-    components.erase(std::remove(components.begin(), components.end(), cpt),
-                     components.end());
-}
-
-void GameObject::AddComponent(Component* cpt) { components.emplace_back(cpt); }
-
-void GameObject::CopyPosition(const GameObject& go) {
-    box = go.box;
-    world_reference = go.world_reference;
-    fromPlayer = go.fromPlayer;
-    angleDeg = go.angleDeg;
-}
-
-void GameObject::start() {
-    if (started) return;
+    else
+    {
+        //Nothing to do
+    }
     for (auto c : components) c->start();
 
     started = true;
+}
+
+bool GameObject::canEnd() const { return true; }
+
+void GameObject::update(float dt)
+{
+    static float sum;
+    assert(sum >= 0);
+    sum += dt;
+    for (Component* component : components) component->update(dt);
+    if (move)
+    {
+        box.pos = tmp.pos + Vec2(0, 1) * sin(sum) * 30;
+    }
+    else
+    {
+        //Nothing to do
+    }
+}
+
+void GameObject::rhythmUpdate()
+{
+    for (Component* component : components) component->rhythmUpdate();
+}
+
+void GameObject::rhythmReset()
+{
+    for (Component* component : components) component->rhythmReset();
+}
+
+void GameObject::renderOrder(Common::Layer layer) const
+{
+    if (!(this->layer & layer))
+    {
+        return;
+    }
+    else
+    {
+        //Nothing to do
+    }
+    for (Component* component : components) component->render(layer);
+}
+
+/**
+* @brief Removing blocks when Alan digs it
+* @param [in] cpt is a Component pointer
+*/
+void GameObject::removeComponent(Component* cpt)
+{
+    components.erase(std::remove(components.begin(), components.end(), cpt),
+    components.end());
+}
+
+void GameObject::addComponent(Component* cpt) { components.emplace_back(cpt); }
+
+void GameObject::copyPosition(const GameObject& go)
+{
+    assert(go != nullptr);
+    box = go.box;
+    world_reference = go.world_reference;
+    from_player = go.from_player;
+    angle_deg = go.angle_deg;
 }
--- src/Music.cpp
+++ src/Music.cpp
@@ -1,8 +1,34 @@
-#include "Music.h"
+/**
+* @file Music.cpp
+* @copyright 2018 Pedro H.
+* @brief Class responsible for playing the game song for each state of the game.
+*/
 
-void Music::Play(int times) const {
-    if (music) {
+#include "Music.h"
+#include <assert.h>
+
+/**
+* @brief Control if the music is played
+* @param [in] times
+* @return void
+* @details Music is set off if times = -1
+*/
+void Music::Play(int times) const
+{
+    assert(times > 0);
+    if (music)
+    {
         if (Mix_PlayMusic(music.get(), times) == -1)
+        {
             std::cerr << "Mix_PlayMusic: " << Mix_GetError() << std::endl;
+        }
+        else
+        {
+            //Nothing to do
+        }
+    }
+    else
+    {
+        //Nothing to do
     }
 }
